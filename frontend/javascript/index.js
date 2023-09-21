2ca17ad2536916c203298fe29cbd0d855b0c46ed
Matt Buck <matt@voxable.io>
2023-09-21 17:22:47 -0400

    Add annotation plugin

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,7 +1,48 @@
-import "$styles/index.css"
-import "$styles/syntax-highlighting.css"
+import "$styles/index.css";
+import "$styles/syntax-highlighting.css";
 
 // Import all JavaScript & CSS files from src/_components
-import components from "$components/**/*.{js,jsx,js.rb,css}"
+import components from "$components/**/*.{js,jsx,js.rb,css}";
 
-console.info("Bridgetown is loaded!")
+// Import third-party libs.
+import { annotate, annotationGroup } from "rough-notation";
+
+console.info("Bridgetown is loaded!");
+
+// Animate annotations.
+const softwareText = document.querySelector("#text-software");
+const softwareAnnotation = annotate(softwareText, {
+  type: "underline",
+  color: "#f9d423",
+});
+const startupsText = document.querySelector("#text-startups");
+const startupsAnnotation = annotate(startupsText, {
+  type: "underline",
+  color: "#00ff00",
+});
+
+const conversationalText = document.querySelector("#text-conversational");
+const conversationalAnnotation = annotate(conversationalText, {
+  type: "bracket",
+  color: "#00ffff",
+});
+
+const nameText = document.querySelector("#text-name");
+const nameAnnotation = annotate(nameText, {
+  type: "highlight",
+  color: "rgba(255, 0, 255, 0.1)",
+});
+const voxableText = document.querySelector("#text-voxable");
+const voxableAnnotation = annotate(voxableText, {
+  type: "highlight",
+  color: "rgba(255, 0, 255, 0.1)",
+});
+
+const ag = annotationGroup([
+  softwareAnnotation,
+  startupsAnnotation,
+  conversationalAnnotation,
+  nameAnnotation,
+  voxableAnnotation,
+]);
+ag.show();
--- src/index.md
+++ src/index.md
@@ -4,6 +4,6 @@
 layout: default
 ---
 
-# I build software & startups, with a focus on conversational & generative AI.
+# I build <span id="text-software">software</span> & <span id="text-startups">startups</span>, with a focus on <span id="text-conversational">conversational & generative AI.</span>
 
-I'm a software engineer, entrepreneur, and technology educator. I built [Voxable](https://voxable.io), spoke at SXSW, helped launch a coding bootcamp, and worked as a full-stack engineer for a number of software companies across my nearly two decades in the industry.
+I'm <span id="text-name">Matt Buck</span>, a software engineer, entrepreneur, and technology educator living in Pittsburgh. I built <span id="text-voxable">[Voxable](https://voxable.io)</span>, spoke at SXSW, helped launch a coding bootcamp, and worked as a full-stack engineer for a number of software companies across my nearly two decades in the industry.
