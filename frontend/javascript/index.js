a21d044c8f6e167eb76ecaabce4207d0bb8af69d
Matt Buck <matt@voxable.io>
2023-09-22 14:35:02 -0400

    Run annotations on doc load

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -9,40 +9,43 @@ import { annotate, annotationGroup } from "rough-notation";
 
 console.info("Bridgetown is loaded!");
 
-// Animate annotations.
-const softwareText = document.querySelector("#text-software");
-const softwareAnnotation = annotate(softwareText, {
-  type: "underline",
-  color: "#f9d423",
-});
-const startupsText = document.querySelector("#text-startups");
-const startupsAnnotation = annotate(startupsText, {
-  type: "underline",
-  color: "#00ff00",
-});
+// on document load
+document.addEventListener("DOMContentLoaded", () => {
+  // Animate annotations.
+  const softwareText = document.querySelector("#text-software");
+  const softwareAnnotation = annotate(softwareText, {
+    type: "underline",
+    color: "#f9d423",
+  });
+  const startupsText = document.querySelector("#text-startups");
+  const startupsAnnotation = annotate(startupsText, {
+    type: "underline",
+    color: "#00ff00",
+  });
 
-const conversationalText = document.querySelector("#text-conversational");
-const conversationalAnnotation = annotate(conversationalText, {
-  type: "bracket",
-  color: "#00ffff",
-});
+  const conversationalText = document.querySelector("#text-conversational");
+  const conversationalAnnotation = annotate(conversationalText, {
+    type: "bracket",
+    color: "#00ffff",
+  });
 
-const nameText = document.querySelector("#text-name");
-const nameAnnotation = annotate(nameText, {
-  type: "highlight",
-  color: "rgba(255, 0, 255, 0.1)",
-});
-const voxableText = document.querySelector("#text-voxable");
-const voxableAnnotation = annotate(voxableText, {
-  type: "highlight",
-  color: "rgba(255, 0, 255, 0.1)",
-});
+  const nameText = document.querySelector("#text-name");
+  const nameAnnotation = annotate(nameText, {
+    type: "highlight",
+    color: "rgba(255, 0, 255, 0.1)",
+  });
+  const voxableText = document.querySelector("#text-voxable");
+  const voxableAnnotation = annotate(voxableText, {
+    type: "highlight",
+    color: "rgba(255, 0, 255, 0.1)",
+  });
 
-const ag = annotationGroup([
-  softwareAnnotation,
-  startupsAnnotation,
-  conversationalAnnotation,
-  nameAnnotation,
-  voxableAnnotation,
-]);
-ag.show();
+  const ag = annotationGroup([
+    softwareAnnotation,
+    startupsAnnotation,
+    conversationalAnnotation,
+    nameAnnotation,
+    voxableAnnotation,
+  ]);
+  ag.show();
+});
