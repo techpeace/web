1bb92b61463116fd2d3ab1741f74c86c227fb1fa
Matt Buck <matt@voxable.io>
2023-11-07 15:27:38 -0500

    Enable use of Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.md", "./src/**/*.liquid"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
